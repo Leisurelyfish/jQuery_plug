a287e2aecf2a534a03f9b460489f8e7cbd142ba3
king <king_20150123@163.com>
2019-05-16 23:38:58 +0800

    init plug

--- validator.js
+++ validator.js
@@ -1,1 +1,7 @@
-valid
+;(function(global, factory, plug){
+  return factory.call(global, global.jQuery, plug);
+})(this,function($, validator){
+  $.fn[validator] = function() {
+
+  }
+}, 'bootstrapValidator')
