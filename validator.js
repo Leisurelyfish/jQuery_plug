8338b8f244503c39f8102a2fc31e2b0becaae397
king <king_20150123@163.com>
2019-05-19 21:43:39 +0800

    modify file validator.js

--- validator.js
+++ validator.js
@@ -1,7 +1,41 @@
 ;(function(global, factory, plug){
-  return factory.call(global, global.jQuery, plug);
-})(this,function($, validator){
-  $.fn[validator] = function() {
-
+  factory.call(global, global.jQuery, plug);
+})(this, function($, validator){
+  // 默认值常量
+  var __DEFS__ = {
+    raise: "change", //最常用的设为默认值
   }
-}, 'bootstrapValidator')
+
+  // 校验规则
+  var __RULES__ = {
+    require: function(){
+      const val = this.val();
+      return val!=""&&val!=null&&val!=undefined; 
+    },
+    email: function(){
+      return /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/.text(this.val());
+    },
+    regex: function(val){
+      
+    }
+  }
+  $.fn[validator] = function(ops){
+    if(!this.is('form')){
+      throw new Error("Type error[require form tag]");
+    }
+    this.$fields = this.find("input,textarea,select")
+      .not("input[type=button],input[type=submit],input[type=reset],input[type=image]");
+    $.extend(this, __DEFS__, ops);
+    this.$fields.on(this.raise, function(){
+      var $fields = $(this);
+      // console.log($(this))
+      var __err__ = true; //校验结果默认为rue
+      $.each(__RULES__, function(rule, valid){
+        if($fields.data("bv-" + rule)){
+          __err__ = valid.call($fields);
+          return __err__;
+        }
+      })
+    })
+  }
+}, 'bootstrapValidate')
