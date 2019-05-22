c534869fb2165da653f37ef514e964cd6999e6ab
king <king_20150123@163.com>
2019-05-22 22:51:45 +0800

    completed

--- validator.js
+++ validator.js
@@ -4,6 +4,10 @@
   // 默认值常量
   var __DEFS__ = {
     raise: "change", //最常用的设为默认值
+    errorMsg: "* 校验失败",
+    extendRule: function(rule){
+      $.extend(__RULES__, rule);
+    }
   }
 
   // 校验规则
@@ -13,10 +17,10 @@
       return val!=""&&val!=null&&val!=undefined; 
     },
     email: function(){
-      return /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/.text(this.val());
+      return /^[A-Za-z0-9._%-]+@([A-Za-z0-9-]+\.)+[A-Za-z]{2,4}$/.test(this.val());
     },
-    regex: function(val){
-      
+    regexp: function(config){
+      return new RegExp(config).test(this.val());
     }
   }
   $.fn[validator] = function(ops){
@@ -26,16 +30,26 @@
     this.$fields = this.find("input,textarea,select")
       .not("input[type=button],input[type=submit],input[type=reset],input[type=image]");
     $.extend(this, __DEFS__, ops);
+    var that = this;
     this.$fields.on(this.raise, function(){
       var $fields = $(this);
-      // console.log($(this))
-      var __err__ = true; //校验结果默认为rue
+      var $group = $fields.parent(".form-group");//父元素
+      $group.removeClass('has-success has-error');
+      $group.find('.help-block').remove();
+      var config,error,__err__ = true; //校验结果默认为rue
       $.each(__RULES__, function(rule, valid){
-        if($fields.data("bv-" + rule)){
-          __err__ = valid.call($fields);
+        config = $fields.data("bv-" + rule);
+        if(config){
+          __err__ = valid.call($fields, config);
+          $group.addClass(__err__?'has-success':'has-error');
+          if(!__err__){
+            error = $fields.data('bv-'+rule+'-error')|| that.errorMsg;
+            $fields.after('<span class="help-block">'+error+'</span>')
+          }
           return __err__;
         }
       })
     })
+    return this;
   }
 }, 'bootstrapValidate')
